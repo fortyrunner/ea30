6404724208ac77d5c060cacc165f25e1bc98449e
john@runfast.co.uk <aFYsYVvqZAKnYgf;dHrNDVxmYWHexZYwzB9J>
2021-05-31 16:54:52 +0100

    Add separate parsing module to dump CSV

--- ea30/parser.py
+++ ea30/parser.py
@@ -1,14 +1,17 @@
 import json
+import csv
 
-print("week,date,first,last,cat,catpos,distance")
-for i in range(1, 22):
-    with open("output/rawfiles/ea{:02}.json".format(i), "r") as my_file:
-        contents = my_file.read()
-        parsed = json.loads(contents)
-        eventName = parsed["event_name"]
-        results = parsed["results"]
-        for result in results:
-            team = result["team"]
-            if team == "ARE80":
-                distance = result["athlon_points"]
-                print(i, ",", eventName, ",", result["first_name"], result["last_name"], ",", result["category"], ",", result["catpos"], ",", distance)
+with open('output/results-2021.csv', 'w', newline='') as output_file:
+    csv_file = csv.writer(output_file)
+    csv_file.writerow(["Week", "Date", "Name", "Place", "Category", "Category Position", "Distance"])
+    for i in range(1, 23):
+        with open('output/rawfiles/ea{:02}.json'.format(i), "r") as my_file:
+            contents = my_file.read()
+            parsed = json.loads(contents)
+            eventName = parsed["event_name"]
+            results = parsed["results"]
+            for result in results:
+                if result["team"] == "ARE80":
+                    distance = result["athlon_points"]
+                    csv_file.writerow([i, eventName, result["first_name"] + " " + result["last_name"], result["place"],
+                                       result["category"], result["catpos"], distance])
