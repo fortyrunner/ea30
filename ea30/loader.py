649461ca6c1ca14fe1bdf2b33b3bbab6d9661034
john@runfast.co.uk <aFYsYVvqZAKnYgf;dHrNDVxmYWHexZYwzB9J>
2021-05-31 21:59:30 +0100

    Add 2020 support

--- ea30/loader.py
+++ ea30/loader.py
@@ -1,9 +1,16 @@
 import requests
 
-url = "https://data.opentrack.run/en-gb/x/2021/GBR/weekly30/event/R{:02}/1/1/json/"
+url2021 = 'https://data.opentrack.run/en-gb/x/2021/GBR/weekly30/event/R{:02}/1/1/json/'
+url2020 = 'https://data.opentrack.run/en-gb/x/2020/GBR/weekly30/event/R{:02}/1/1/json'
 
-for i in range(22, 23):
-    resp = requests.get(url.format(i))
-    with open("output/rawfiles/ea{:02}.json".format(i), "w") as my_file:
+for i in range(1, 23):
+    resp = requests.get(url2021.format(i))
+    with open("output/rawfiles/ea-2021-{:02}.json".format(i), "w") as my_file:
+        print("Reading week ", i)
+        my_file.write(resp.text)
+
+for i in range(1, 27):
+    resp = requests.get(url2020.format(i))
+    with open("output/rawfiles/ea-2020-{:02}.json".format(i), "w") as my_file:
         print("Reading week ", i)
         my_file.write(resp.text)
--- ea30/parser.py
+++ ea30/parser.py
@@ -1,17 +1,20 @@
 import json
 import csv
 
-with open('output/results-2021.csv', 'w', newline='') as output_file:
-    csv_file = csv.writer(output_file)
-    csv_file.writerow(["Week", "Date", "Name", "Place", "Category", "Category Position", "Distance"])
-    for i in range(1, 23):
-        with open('output/rawfiles/ea{:02}.json'.format(i), "r") as my_file:
-            contents = my_file.read()
-            parsed = json.loads(contents)
-            eventName = parsed["event_name"]
-            results = parsed["results"]
-            for result in results:
-                if result["team"] == "ARE80":
-                    distance = result["athlon_points"]
-                    csv_file.writerow([i, eventName, result["first_name"] + " " + result["last_name"], result["place"],
-                                       result["category"], result["catpos"], distance])
+def parse(year, end):
+    with open('output/results-{:04}.csv'.format(year), 'w', newline='') as output_file:
+        csv_file = csv.writer(output_file)
+        csv_file.writerow(["Week", "Date", "Name", "Place", "Category", "Category Position", "Distance"])
+        for i in range(1, end):
+            with open('output/rawfiles/ea-{:04}-{:02}.json'.format(year, i), "r") as my_file:
+                contents = my_file.read()
+                parsed = json.loads(contents)
+                eventName = parsed["event_name"]
+                results = parsed["results"]
+                for result in results:
+                    if result["team"] == "ARE80":
+                        distance = result["athlon_points"]
+                        csv_file.writerow([i, eventName, result["first_name"] + " " + result["last_name"], result["place"],
+                                           result["category"], result["catpos"], distance])
+parse(2021, 23)
+parse(2020, 27)
