c21bec69113b3b9c58d713f1436dff539aa83403
john@runfast.co.uk <aFYsYVvqZAKnYgf;dHrNDVxmYWHexZYwzB9J>
2021-06-01 08:24:50 +0100

    Create combined file

--- ea30/parser.py
+++ ea30/parser.py
@@ -1,7 +1,7 @@
 import json
 import csv
 
-def parse(year, end):
+def parse(year, end, totals_file):
     with open('output/results-{:04}.csv'.format(year), 'w', newline='') as output_file:
         csv_file = csv.writer(output_file)
         csv_file.writerow(["Year", "Week", "Date", "Name", "Place", "Category", "Category Position", "Distance"])
@@ -16,5 +16,11 @@ def parse(year, end):
                         distance = result["athlon_points"]
                         csv_file.writerow([year, i, eventName, result["first_name"] + " " + result["last_name"], result["place"],
                                            result["category"], result["catpos"], distance])
-parse(2021, 23)
-parse(2020, 27)
+                        totals_file.writerow([year, i, eventName, result["first_name"] + " " + result["last_name"], result["place"],
+                                           result["category"], result["catpos"], distance])
+
+with open('output/combined-results.csv', 'w', newline='') as results_file:
+    totals_file = csv.writer(results_file)
+    totals_file.writerow(["Year", "Week", "Date", "Name", "Place", "Category", "Category Position", "Distance"])
+    parse(2020, 27, totals_file)
+    parse(2021, 23, totals_file)
