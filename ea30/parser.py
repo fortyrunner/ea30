39d99a8a585ec01dfca75768f465875711d6eee8
john@runfast.co.uk <aFYsYVvqZAKnYgf;dHrNDVxmYWHexZYwzB9J>
2021-06-02 16:51:17 +0100

    Refactor

--- ea30/parser.py
+++ ea30/parser.py
@@ -1,6 +1,9 @@
 import json
 import csv
 
+HEADER = ["Year", "Week", "Date", "Name", "Place", "Category", "Category Position", "Distance"]
+
+
 def write_data(year, i, event_name, result, csv_file, totals_file):
     distance = result["athlon_points"]
     row = [year, i, event_name, result["first_name"] + " " + result["last_name"],
@@ -12,7 +15,7 @@ def write_data(year, i, event_name, result, csv_file, totals_file):
 def parse(year, end, totals_file, team):
     with open('output/results-{:04}.csv'.format(year), 'w', newline='') as output_file:
         csv_file = csv.writer(output_file)
-        csv_file.writerow(["Year", "Week", "Date", "Name", "Place", "Category", "Category Position", "Distance"])
+        csv_file.writerow(HEADER)
         for i in range(1, end):
             with open('output/rawfiles/ea-{:04}-{:02}.json'.format(year, i), "r") as my_file:
                 contents = my_file.read()
@@ -24,9 +27,8 @@ def parse(year, end, totals_file, team):
                         write_data(year, i, event_name, result, csv_file, totals_file)
 
 
-
 with open('output/combined-results.csv', 'w', newline='') as results_file:
     totals_file = csv.writer(results_file)
-    totals_file.writerow(["Year", "Week", "Date", "Name", "Place", "Category", "Category Position", "Distance"])
+    totals_file.writerow(HEADER)
     parse(2020, 27, totals_file, "ARE80")
     parse(2021, 23, totals_file, "ARE80")
